5c37b13b3b08a02a9038351fbb88471a501fde7a
fekaiq <kaiquefe@dcc.ufrj.br>
2025-06-28 20:02:45 -0300

    Adicionei migração inicial e ajustei models, settings e requirements

--- emendas_app/admin.py
+++ emendas_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+    Proponente, Funcao, Localidade,
+    Instituicao, Emenda, AcaoOrcamentaria, Beneficio
+)
 
-# Register your models here.
+admin.site.register(Proponente)
+admin.site.register(Funcao)
+admin.site.register(Localidade)
+admin.site.register(Instituicao)
+admin.site.register(Emenda)
+admin.site.register(AcaoOrcamentaria)
+admin.site.register(Beneficio)
+
--- emendas_app/models.py
+++ emendas_app/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+class Proponente(models.Model):
+    cod_proponente = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Funcao(models.Model):
+    cod_funcao = models.IntegerField(primary_key=True)
+    subfuncao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.subfuncao
+
+
+class Localidade(models.Model):
+    id_local = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    municipio = models.CharField(max_length=100)
+    uf = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.nome} - {self.uf}"
+
+
+class Instituicao(models.Model):
+    cod_instituicao = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50)
+    id_local = models.ForeignKey(Localidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+class Emenda(models.Model):
+    cod_emenda = models.IntegerField(primary_key=True)
+    tipo = models.CharField(max_length=50)
+    codigo_funcao = models.ForeignKey(Funcao, on_delete=models.CASCADE)
+    codigo_proponente = models.ForeignKey(Proponente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Emenda {self.cod_emenda}"
+
+
+class AcaoOrcamentaria(models.Model):
+    cod_acao = models.IntegerField(primary_key=True)
+    descricao = models.CharField(max_length=150)
+    codigo_emenda = models.ForeignKey(Emenda, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Beneficio(models.Model):
+    codigo_emenda = models.ForeignKey(Emenda, on_delete=models.CASCADE)
+    codigo_instituicao = models.ForeignKey(Instituicao, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('codigo_emenda', 'codigo_instituicao'),)
+
+    def __str__(self):
+        return f"Benefício: Emenda {self.codigo_emenda_id} - Instituição {self.codigo_instituicao_id}"
+
